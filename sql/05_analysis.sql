3a26d421a87a25cc9fd6b5222c7f0b02d4c40268
Ahmad Wishnu <ahmadwishnu@gmail.com>
2025-10-22 23:16:47 +0700

    Create 05_analysis.sql

---
+++ sql/05_analysis.sql
@@ -0,0 +1,57 @@
+-- 1. Overview statistics
+SELECT 
+  COUNT(*) AS total_records,
+  ROUND(AVG(age),2) AS avg_age,
+  ROUND(AVG(bmi),2) AS avg_bmi,
+  ROUND(AVG(sleep),2) AS avg_sleep,
+  ROUND(SUM(CASE WHEN health_risk='high' THEN 1 ELSE 0 END)*100.0/COUNT(*),2) AS pct_high_risk
+FROM health_data_clean;
+
+-- 2. Health risk by exercise level
+SELECT exercise,
+       COUNT(*) AS total_people,
+       ROUND(SUM(CASE WHEN health_risk='high' THEN 1 ELSE 0 END)*100.0/COUNT(*),2) AS high_risk_pct
+FROM health_data_clean
+GROUP BY exercise
+ORDER BY high_risk_pct DESC;
+
+-- 3. Smoking + alcohol combinations
+SELECT smoking, alcohol,
+       COUNT(*) AS total,
+       ROUND(SUM(CASE WHEN health_risk='high' THEN 1 ELSE 0 END)*100.0/COUNT(*),2) AS high_risk_pct
+FROM health_data_clean
+GROUP BY smoking, alcohol
+ORDER BY high_risk_pct DESC;
+
+-- 4. BMI category & health risk
+SELECT 
+  CASE 
+    WHEN bmi < 18.5 THEN 'Underweight'
+    WHEN bmi BETWEEN 18.5 AND 24.9 THEN 'Normal'
+    WHEN bmi BETWEEN 25 AND 29.9 THEN 'Overweight'
+    ELSE 'Obese'
+  END AS bmi_category,
+  COUNT(*) AS total,
+  ROUND(SUM(CASE WHEN health_risk='high' THEN 1 ELSE 0 END)*100.0/COUNT(*),2) AS high_risk_pct
+FROM health_data_clean
+GROUP BY bmi_category
+ORDER BY high_risk_pct DESC;
+
+-- 5. Health risk by profession
+SELECT profession,
+       COUNT(*) AS total_people,
+       ROUND(SUM(CASE WHEN health_risk='high' THEN 1 ELSE 0 END)*100.0/COUNT(*),2) AS high_risk_pct
+FROM health_data_clean
+GROUP BY profession
+ORDER BY high_risk_pct DESC
+LIMIT 10;
+
+-- 6. Simple lifestyle risk score
+SELECT *,
+    (CASE WHEN exercise='high' THEN 0 WHEN exercise='medium' THEN 1 ELSE 2 END +
+     CASE WHEN sleep>=7 THEN 0 ELSE 1 END +
+     CASE WHEN sugar_intake='high' THEN 1 ELSE 0 END +
+     CASE WHEN smoking='yes' THEN 1 ELSE 0 END +
+     CASE WHEN alcohol='yes' THEN 1 ELSE 0 END) AS lifestyle_risk_score
+FROM health_data_clean
+LIMIT 10;
